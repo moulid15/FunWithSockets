2ea1be2ba480463f83fd71fb2e4dff047986430f
moulid15 <moulidah@buffalo.edu>
2019-12-27 12:51:51 -0500

    update

--- problem1.c
+++ problem1.c
@@ -9,15 +9,18 @@
 #include <time.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
+
 int main(int argc , char *argv[])
 {
+
 	int socket_desc;
+	int ret;
 	struct sockaddr_rc server={0};
 	char *message , server_reply[2000];
 	char *Mac = "AC:ED:5C:B4:3D:5D";
-
 	//Create socket
-  while(1){
+
+
 	socket_desc = socket(AF_BLUETOOTH , SOCK_STREAM , BTPROTO_RFCOMM);
 	if (socket_desc == -1)
 	{
@@ -27,7 +30,7 @@ int main(int argc , char *argv[])
 	server.rc_family = AF_BLUETOOTH;
 	server.rc_channel = (uint8_t)1;
 	str2ba(Mac,&server.rc_bdaddr);
-
+	printf("yess....\n");
 
 	//Connect to remote server
 
@@ -36,23 +39,57 @@ int main(int argc , char *argv[])
   if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
   {
 
-    puts("connect error1");
-  }
-	//Send some data
+    puts("connect error1\n");
+		return 1;
 
-    if(recv(socket_desc, &server_reply , 2000 , 0)<0){
+  }
+	// printf("get out connect\n" );
+	//Send some data
+		// connect(socket_desc , (struct sockaddr *)&server , sizeof(server));
+
+while(1){
+		// memset(server_reply ,0 , 2000);
+		puts("again");
+    if((ret=(recv(socket_desc, server_reply , 2000,0 )))==0){
       return 1;
-    }else{
+    }else if(ret < 0){
+			puts("yes");
+			close(socket_desc);
+			return 2;
+		}else{
     puts("Reply received\n");
   	puts(server_reply);
-    memset(server_reply ,0 , 2000);
-		sleep(3);
+		puts("print this");
+		socket_desc = socket(AF_BLUETOOTH , SOCK_STREAM , BTPROTO_RFCOMM);
+		if (socket_desc == -1)
+		{
+			printf("Could not create socket");
+		}
 
+		server.rc_family = AF_BLUETOOTH;
+		server.rc_channel = (uint8_t)1;
+		str2ba(Mac,&server.rc_bdaddr);
+		// sleep(3);
 	}
-}
+	connect(socket_desc , (struct sockaddr *)&server , sizeof(server));
+		memset(server_reply ,0 , 2000);
+		puts("memset");
+	}
+// 		if(recv(socket_desc, server_reply , 2000,0 )<0){
+// 			puts("nothing");
+//       return 1;
+//     }else{
+//     puts("Reply received\n");
+//   	puts(server_reply);
+//
+//
+//
+// 	// }
+// 	// puts("connect error");
+// }
 
 puts("recv failed");
-puts("connect error");
+
 // close(socket_desc);
 	return 0;
 }
